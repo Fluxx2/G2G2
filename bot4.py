130b97d6597a6dcc0d30cdcbf33f0c2c36a249f4
Fluxx2 <sanankhan98921@gmail.com>
2026-01-14 10:19:33 +0530

    Update bot4.py

--- bot4.py
+++ bot4.py
@@ -11,8 +11,8 @@ from itertools import product
 SOURCE_CHANNEL_ID = 1442370325831487608
 TARGET_CHANNEL_ID = 1449692284596523068
 
-MAX_AGE_SECONDS = 225
-TOGGLE_INTERVAL = 28
+MAX_AGE_SECONDS = 210
+TOGGLE_INTERVAL = 18
 EXPIRY_CHECK_INTERVAL = 3
 
 MAX_EDIT_AGE_SECONDS = 55 * 60  # rotate before Discord 1h edit limit
@@ -257,3 +257,4 @@ async def on_message_delete(message):
 # RUN
 # ================================
 client.run(TOKEN)
+
